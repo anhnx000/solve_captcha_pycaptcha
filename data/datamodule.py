b020596c2a1047eec547d4084aaf44d62e8557a5
anhnx000 <xuananhbka@gmail.com>
2025-05-13 12:08:47 +0930

    anhnx000 add token 63

--- data/datamodule.py
+++ data/datamodule.py
@@ -27,18 +27,21 @@ class captcha_dm(LightningDataModule):
         self.num_workers = num_workers
 
     def setup(self, stage:  Optional[str]) -> None:
-        train_dataset_1  = captcha_dataset('train')
-        train_dataset_2  = captcha_dataset('../dataset_real/train')
-        train_dataset_3 = torch.utils.data.ConcatDataset([train_dataset_2] * 250)
-        self.train_dataset = torch.utils.data.ConcatDataset([train_dataset_1, train_dataset_3])
+        # train_dataset_1  = captcha_dataset('train')
+        # train_dataset_2  = captcha_dataset('../dataset_real/train')
+        # train_dataset_3 = torch.utils.data.ConcatDataset([train_dataset_2] * 250)
+        # self.train_dataset = torch.utils.data.ConcatDataset([train_dataset_1, train_dataset_3])
         
-        val_dataset_1  = captcha_dataset('val')
-        val_dataset_2  = captcha_dataset('../dataset_real/val')
-        self.val_dataset = torch.utils.data.ConcatDataset([val_dataset_1, val_dataset_2])
+        # val_dataset_1  = captcha_dataset('val')
+        # val_dataset_2  = captcha_dataset('../dataset_real/val')
+        # self.val_dataset = torch.utils.data.ConcatDataset([val_dataset_1, val_dataset_2])
         
-        # test_dataset_1  = captcha_dataset('test')
-        # test_dataset_2  = captcha_dataset('../dataset_real/test')
-        # self.test_dataset = torch.utils.data.ConcatDataset([test_dataset_1, test_dataset_2])
+        # # test_dataset_1  = captcha_dataset('test')
+        # # test_dataset_2  = captcha_dataset('../dataset_real/test')
+        # # self.test_dataset = torch.utils.data.ConcatDataset([test_dataset_1, test_dataset_2])
+        
+        self.train_dataset = captcha_dataset('train')
+        self.val_dataset = captcha_dataset('val')        
     
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True, collate_fn=collate_fn_ignore_errors)
